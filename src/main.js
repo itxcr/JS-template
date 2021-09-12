bf58e5485a70c66bed941fe96c59b0a252cdb095
xcr <itxcr@protonmail.com>
2021-09-12 11:51:44 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,8 +4,24 @@ import rotate from '@/assets/img/rotate.png'
 const div = document.createElement('img')
 div.classList.add('app')
 div.src = rotate
+console.log(close)
 
 const body = document.getElementsByTagName('body')[0]
 body.style.backgroundColor = '#000000'
 
 body.appendChild(div)
+
+class Test {
+  constructor(name) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
+
+}
+
+const b = new Test('xcr')
+console.log(b)
+console.log(b.name)
